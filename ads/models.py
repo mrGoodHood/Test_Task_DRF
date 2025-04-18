d0262b5b14f176a21e33e1e006724c7418aee702
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-04-18 16:13:43 +0300

    Add models

---
+++ ads/models.py
@@ -0,0 +1,36 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+
+class Ad(models.Model):
+    """Модель объявления"""
+    CONDITION_CHOICES = [
+        ('new', 'Новый'),
+        ('used', 'Б/У')
+    ]
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    image_url = models.URLField(blank=True, null=True)
+    category = models.CharField(max_length=100)
+    condition = models.CharField(max_length=10, choices=CONDITION_CHOICES)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+
+class ExchangeProposal(models.Model):
+    """Медель предложения обмена между двумя объявлениями"""
+    STATUS_CHOICES = [
+        ('pending', 'Ожидает'),
+        ('accepted', 'Принята'),
+        ('rejected', 'Отклонена'),
+    ]
+
+    ad_sender = models.ForeignKey(Ad, on_delete=models.CASCADE, related_name='sent_proposals')
+    ad_receiver = models.ForeignKey(Ad, on_delete=models.CASCADE, related_name='received_proposals')
+    comment = models.TextField()
+    status = models.CharField(max_length=10, choices=STATUS_CHOICES, default='pending')
+    created_at = models.DateTimeField(auto_now_add=True)
