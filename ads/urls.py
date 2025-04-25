d26ac7e2b66be89cf7ca2ca87a2775858c50b8a9
Andrey Zhidkov <mr.goodhood@gmail.com>
2025-04-25 23:24:43 +0300

    URLs update

--- ads/urls.py
+++ ads/urls.py
@@ -1,5 +1,13 @@
-from django.urls import path
+from django.urls import path, include
+from rest_framework.routers import DefaultRouter
 from . import views
+from .views import AdViewSet, ExchangeProposalViewSet
+
+
+# Роутер DRF
+router = DefaultRouter()
+router.register(r'ads', AdViewSet, basename='ad')
+router.register(r'exchange-proposals', ExchangeProposalViewSet, basename='exchangeproposal')
 
 urlpatterns = [
     path('', views.home, name='home'),
@@ -11,4 +19,5 @@ urlpatterns = [
     path('proposals/create/', views.create_proposal, name='create_proposal'),
     path('proposals/<int:proposal_id>/update/', views.update_proposal, name='update_proposal'),
     path('proposals/', views.view_proposals, name='view_proposals'),
+    path('api/', include(router.urls)),
 ]
